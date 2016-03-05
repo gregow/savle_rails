75a8a22861fd9c0a8a8c4ea20f0a489f6cf87b55
Prelang Builder <builder@prelang.com>
2016-03-05 02:27:48 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Camiseta' to allow for create, update

--- app/admin/camiseta.rb
+++ app/admin/camiseta.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Camiseta do
+  permit_params :nome, :medida_1
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
